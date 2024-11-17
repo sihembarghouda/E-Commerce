e6918e7fc7026220090ec28405eeed47e5e8069d
Barghouda Sihem <sihembarghouda93@gmail.com>
2024-11-17 20:56:43 +0100

    Premier commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { SaleComponent } from './pages/sale/sale.component';
+
+const routes: Routes = [
+  {
+    path:'products',
+    component: HomeComponent
+  },
+  {
+    path:'cart',
+    component: CartComponent
+  },
+  {
+    path:"sale",
+    component: SaleComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { SaleComponent } from './pages/sale/sale.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CartComponent,
+    SaleComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

49e8412ecf9c84a6dd8cab9c6bea1d5110ec352b
Barghouda Sihem <sihembarghouda93@gmail.com>
2024-11-28 20:16:24 +0100

    nouveaux modifications

--- src/app/pages/clothes/clothes.component.ts
+++ src/app/pages/clothes/clothes.component.ts
@@ -1,10 +1,32 @@
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-clothes',
   templateUrl: './clothes.component.html',
-  styleUrl: './clothes.component.css'
+  styleUrls: ['./clothes.component.css']
 })
 export class ClothesComponent {
+  clothes = [
+    {
+      name: 'T-shirt blanc',
+      price: 19.99,
+      image: 'assets/images/tshirt-blanc.jpg'
+    },
+    {
+      name: 'Jean bleu',
+      price: 39.99,
+      image: 'assets/images/jean-bleu.jpg'
+    },
+    {
+      name: 'Robe d\'été',
+      price: 49.99,
+      image: 'assets/images/robe-ete.jpg'
+    }
+  ];
 
+  orderItem(item: any) {
+    console.log('Commande passée pour:', item.name);
+    // Logique supplémentaire pour le traitement de la commande
+  }
 }
